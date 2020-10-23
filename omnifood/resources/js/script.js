1a9b2e92b2a1008d1c1694a4a73a22709c9e5faf
Steve <stevewalsh89@gmail.com>
2020-10-23 15:35:48 +0100

    basic jquery media queries for navigation

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
 
-   
+    // For sticky navigation 
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+    offset: '60px;'
+    });
 
 
+    // scroll to area on button click
+    $('.js--scroll-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
 
-
-
-
-
+    $('.js--scroll-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
 
 
 });
